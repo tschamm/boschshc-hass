af5f48e8828f38f78f07d064dcee6dcb65e4d072
Wolfgang Köbele <wolfgang@wkoebele.de>
2020-04-12 18:36:01 +0200

    Add EnergySensor
    * Add class EnergySensor for light controls and smart plugs to read the
    accumulated energy.
    
    * extend functionallity of get_power_sensor_entities to generate also
    this sensor entities and rename it to get_power_sensor_entities

--- boschshc/sensor.py
+++ boschshc/sensor.py
@@ -5,7 +5,7 @@ import logging
 from boschshcpy import SHCSession, services_impl, SHCSmartPlug
 
 from homeassistant.components.sensor import DEVICE_CLASSES
-from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME, TEMP_CELSIUS, POWER_WATT, DEVICE_CLASS_POWER
+from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME, TEMP_CELSIUS, POWER_WATT, DEVICE_CLASS_POWER, ENERGY_WATT_HOUR
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import slugify
 
@@ -33,8 +33,8 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
                 entities += [entity]
 
     ip_address = config_entry.data[CONF_IP_ADDRESS]
-    entities += get_power_sensor_entities(session.device_helper.light_controls, "light control", ip_address, session)
-    entities += get_power_sensor_entities(session.device_helper.smart_plugs, "smart plug", ip_address, session)
+    entities += get_power_energy_sensor_entities(session.device_helper.light_controls, "light control", ip_address, session)
+    entities += get_power_energy_sensor_entities(session.device_helper.smart_plugs, "smart plug", ip_address, session)
 
     if entities:
         return await async_add_entities(entities)
@@ -59,21 +59,22 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                 entities += [entity]
 
     ip_address = config_entry.data[CONF_IP_ADDRESS]
-    entities += get_power_sensor_entities(session.device_helper.light_controls, "light control", ip_address, session)
-    entities += get_power_sensor_entities(session.device_helper.smart_plugs, "smart plug", ip_address, session)
+    entities += get_power_energy_sensor_entities(session.device_helper.light_controls, "light control", ip_address, session)
+    entities += get_power_energy_sensor_entities(session.device_helper.smart_plugs, "smart plug", ip_address, session)
 
     if entities:
         async_add_entities(entities)
 
-def get_power_sensor_entities(controls, name, ip_address, session):
+def get_power_energy_sensor_entities(controls, name, ip_address, session):
     entities = []
     for light in controls:
         _LOGGER.debug(f"Found {name}: {light.name} ({light.id})")
         controller_ip = ip_address
         room_name = session.room(light.room_id).name
-        _LOGGER.debug(f"room name: {room_name}")
-        entity = PowerSensor(controller_ip, room_name, light)
-        entities += [entity]
+        power_sensor = PowerSensor(controller_ip, room_name, light)
+        entities += [power_sensor]
+        energy_sensor = EnergySensor(controller_ip, room_name, light)
+        entities += [energy_sensor]
     return entities
 
 class TemperatureSensor(Entity):
@@ -201,6 +202,82 @@ class PowerSensor(Entity):
     @property
     def unit_of_measurement(self):
         return POWER_WATT
+
+    @property
+    def state_attributes(self):
+        state_attr = super().state_attributes
+        if state_attr is None:
+            state_attr = dict()
+        state_attr["boschshc_room_name"] = self._room_name
+        return state_attr
+
+class EnergySensor(Entity):
+    def __init__(
+        self,
+        controller_ip: str,
+        room_name: str,
+        device: SHCSmartPlug,
+    ):
+        self._controller_ip = controller_ip
+        self._room_name = room_name
+        self._device = device
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+
+        def on_state_changed():
+            self.schedule_update_ha_state()
+
+        for service in self._device.device_services:
+            service.on_state_changed = on_state_changed
+
+    async def async_will_remove_from_hass(self):
+        await super().async_will_remove_from_hass()
+        for service in self._device.device_services:
+            service.on_state_changed = None
+
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def name(self):
+        return f"{self._device.name} Energy"
+
+    @property
+    def unique_id(self):
+        return f"{self._device.serial}_energy"
+
+    @property
+    def state(self):
+        return self._device.energyconsumption
+
+    @property
+    def device_id(self):
+        """Return the ID of this device."""
+        return self._device.id
+
+    @property
+    def manufacturer(self):
+        """The manufacturer of the device."""
+        return self._device.manufacturer
+
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {
+            "identifiers": {(DOMAIN, self.device_id)},
+            "name": self.name,
+            "manufacturer": self.manufacturer,
+            "model": self._device.device_model,
+            "sw_version": "",
+            "via_device": (DOMAIN, self._controller_ip),
+        }
+
+    @property
+    def unit_of_measurement(self):
+        return ENERGY_WATT_HOUR
 
     @property
     def state_attributes(self):
