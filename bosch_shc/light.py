33a25b7ebcb07b4be0a3c82d377aae52a7092a38
Thomas Schamm <thomas@tschamm.de>
2020-11-26 23:23:49 +0100

    Added support for setting the brightness.

--- bosch_shc/light.py
+++ bosch_shc/light.py
@@ -4,7 +4,8 @@ import logging
 from boschshcpy import SHCSession
 
 from homeassistant.components.light import (
-    LightEntity,
+    LightEntity, SUPPORT_COLOR_TEMP, SUPPORT_BRIGHTNESS, ATTR_BRIGHTNESS,
+    ATTR_COLOR_TEMP,
 )
 
 from .const import DOMAIN
@@ -18,7 +19,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     entities = []
     session: SHCSession = hass.data[DOMAIN][config_entry.entry_id]
 
-    for light in session.device_helper.ledvance_lights:
+    for light in session.device_helper.hue_lights:
         room_name = session.room(light.room_id).name
         entities.append(
             LightSwitch(
@@ -32,20 +33,41 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
 class LightSwitch(SHCEntity, LightEntity):
     """Representation of a SHC controlled light."""
+    
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        if self._device.supports_brightness:
+            return SUPPORT_BRIGHTNESS
+        if self._device.supports_color:
+            return SUPPORT_COLOR_TEMP
+        return 0
 
     @property
     def is_on(self):
         """Return light state."""
         return self._device.state
 
+    @property
+    def brightness(self) -> int:
+        """Return the brightness of this light between 0..255."""
+        brightness_value = (
+            round(self._device.brightness * 255 / 100) if self._device.brightness else None
+        )
+        return brightness_value
+
     def turn_on(self, **kwargs):
         """Turn the light on."""
-        self._device.state = True
+        if not self.is_on:
+            self._device.state = True
+
+        brightness = kwargs.get(ATTR_BRIGHTNESS)
+
+        if brightness is None:
+            brightness = self.brightness
+
+        self._device.brightness = round(brightness * 100 / 255)
 
     def turn_off(self, **kwargs):
         """Turn the light off."""
         self._device.state = False
-
-    def toggle(self, **kwargs):
-        """Toggles the light."""
-        self._device.state = not self.is_on
