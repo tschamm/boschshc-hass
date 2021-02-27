fb1ba607975229b668cb7b74c2b7d69fd36a5b66
Thomas Schamm <thomas@tschamm.de>
2021-02-27 22:16:21 +0100

    Removed deprecated alarm control panel.

--- custom_components/bosch_shc/alarm_control_panel.py
+++ custom_components/bosch_shc/alarm_control_panel.py
@@ -29,14 +29,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     session: SHCSession = hass.data[DOMAIN][config_entry.entry_id]
 
-    intrusion_detection_system = session.device_helper.intrusion_detection_system
-    alarm_control_panel = IntrusionDetectionAlarmControlPanel(
-        device=intrusion_detection_system,
-        parent_id=session.information.name,
-        entry_id=config_entry.entry_id,
-    )
-    devices.append(alarm_control_panel)
-
     intrusion_system = session.intrusion_system
     alarm_control_panel = IntrusionSystemAlarmControlPanel(
         device=intrusion_system,
@@ -46,102 +38,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     devices.append(alarm_control_panel)
 
     async_add_entities(devices)
-
-
-class IntrusionDetectionAlarmControlPanel(SHCEntity, AlarmControlPanelEntity):
-    """Representation of SHC intrusion detection control."""
-
-    def __init__(
-        self, device: SHCIntrusionDetectionSystem, parent_id: str, entry_id: str
-    ):
-        super().__init__(device=device, parent_id=parent_id, entry_id=entry_id)
-        self._device_state_attributes = {}
-        _LOGGER.warning(
-            "intrusion detection system deprecated in API version 2.1. The Bosch Smart Home Controller update scheduled for May 2021 will no longer support API 1.x."
-        )
-
-    @property
-    def name(self):
-        """Return the name of this sensor."""
-        return f"{self._device.name} (Deprecated)"
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        if (
-            self._device.alarmstate
-            == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.State.SYSTEM_ARMING
-        ):
-            return STATE_ALARM_ARMING
-        if (
-            self._device.alarmstate
-            == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.State.SYSTEM_DISARMED
-        ):
-            return STATE_ALARM_DISARMED
-        if (
-            self._device.alarmstate
-            == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.State.SYSTEM_ARMED
-        ):
-            if (
-                self._device.alarmprofile
-                == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.Profile.FULL_PROTECTION
-            ):
-                return STATE_ALARM_ARMED_AWAY
-
-            if (
-                self._device.alarmprofile
-                == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.Profile.PARTIAL_PROTECTION
-            ):
-                return STATE_ALARM_ARMED_HOME
-
-            if (
-                self._device.alarmprofile
-                == SHCIntrusionDetectionSystem.IntrusionDetectionControlService.Profile.CUSTOM_PROTECTION
-            ):
-                return STATE_ALARM_ARMED_CUSTOM_BYPASS
-        return None
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the device."""
-        return self._device_state_attributes
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return SUPPORT_ALARM_ARM_AWAY + SUPPORT_ALARM_ARM_HOME
-
-    @property
-    def manufacturer(self):
-        """The manufacturer of the device."""
-        return self._device.manufacturer
-
-    @property
-    def code_format(self):
-        """Return the regex for code format or None if no code is required."""
-        return None
-        # return FORMAT_NUMBER
-
-    @property
-    def code_arm_required(self):
-        """Whether the code is required for arm actions."""
-        return False
-
-    def alarm_disarm(self, code=None):
-        """Send disarm command."""
-        self._device.disarm()
-
-    def alarm_arm_away(self, code=None):
-        """Send arm away command."""
-        self._device.arm()
-
-    def alarm_arm_home(self, code=None):
-        """Send arm home command."""
-        self._device.partial_arm()
-
-    def alarm_trigger(self, code=None):
-        """Send trigger/panic command."""
-        self._device.trigger()
 
 
 class IntrusionSystemAlarmControlPanel(AlarmControlPanelEntity):
