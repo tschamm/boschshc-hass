102f93f769cbd41eaddc5c095ad2e5c1cc807925
Thomas Schamm <thomas@tschamm.de>
2021-10-13 21:27:46 +0200

    Merge changes from core integration. Increased version to 0.4.17

--- custom_components/bosch_shc/config_flow.py
+++ custom_components/bosch_shc/config_flow.py
@@ -71,10 +71,7 @@ def get_info_from_host(hass, host, zeroconf):
         zeroconf,
     )
     information = session.mdns_info()
-    return {
-        "title": information.name,
-        "unique_id": information.unique_id,
-    }
+    return {"title": information.name, "unique_id": information.unique_id}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -190,24 +187,26 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="not_bosch_shc")
 
         try:
-            for host in (
+            hosts = (
                 discovery_info["host"]
                 if isinstance(discovery_info["host"], list)
                 else [discovery_info["host"]]
-            ):
-                if not host.startswith("169."):
-                    break
-            self.info = info = await self._get_info(host)
-            self.host = host
-
+            )
+            for host in hosts:
+                if host.startswith("169."):  # skip link local address
+                    continue
+                self.info = await self._get_info(host)
+                self.host = host
+            if self.host is None:
+                return self.async_abort(reason="cannot_connect")
         except SHCConnectionError:
             return self.async_abort(reason="cannot_connect")
 
         local_name = discovery_info["hostname"][:-1]
         node_name = local_name[: -len(".local")]
 
-        await self.async_set_unique_id(info["unique_id"])
-        self._abort_if_unique_id_configured({CONF_HOST: host})
+        await self.async_set_unique_id(self.info["unique_id"])
+        self._abort_if_unique_id_configured({CONF_HOST: self.host})
         self.context["title_placeholders"] = {"name": node_name}
         return await self.async_step_confirm_discovery()
 
