7571dd5e41cf9a78c3c669e63ae2518b7a7b1038
Thomas Schamm <thomas@tschamm.de>
2023-02-18 23:19:32 +0100

    Report if alarm was triggered.

--- custom_components/bosch_shc/alarm_control_panel.py
+++ custom_components/bosch_shc/alarm_control_panel.py
@@ -12,6 +12,7 @@ from homeassistant.const import (
     STATE_ALARM_ARMED_HOME,
     STATE_ALARM_ARMING,
     STATE_ALARM_DISARMED,
+    STATE_ALARM_TRIGGERED,
     Platform,
 )
 
@@ -126,6 +127,8 @@ class IntrusionSystemAlarmControlPanel(AlarmControlPanelEntity):
                 == SHCIntrusionSystem.Profile.CUSTOM_PROTECTION
             ):
                 return STATE_ALARM_ARMED_CUSTOM_BYPASS
+        if self._device.alarm_state == SHCIntrusionSystem.AlarmState.ALARM_ON:
+            return STATE_ALARM_TRIGGERED
         return None
 
     @property
